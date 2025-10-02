06847f9a72950ed8dea9a84338be784980d261dc
vigneshmadhav <111885443+vigneshmadhav@users.noreply.github.com>
2025-10-02 22:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,5 +191,10 @@ def logout():
 # ----------------------------- #
 # Run App #
 # ----------------------------- #
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT if available
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
